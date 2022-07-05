22348950aa32399d44fa661d03cdcc6c72c0c61a
evgeniy <evgeniy.shmelev.95@mail.ru>
2022-07-05 15:32:09 +0300

    add table user in database

--- src/main/java/ru/Learnup/BookStore/security/SecurityConfig.java
+++ src/main/java/ru/Learnup/BookStore/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package ru.Learnup.BookStore.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,26 +8,33 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+        auth.jdbcAuthentication().dataSource(dataSource);
 
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("user").password("user").roles("USER"))
-                .withUser(userBuilder.username("admin").password("admin").roles("ADMIN"));
+//        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+//
+//        auth.inMemoryAuthentication()
+//                .withUser(userBuilder.username("user").password("12345").roles("USER"))
+//                .withUser(userBuilder.username("admin").password("admin").roles("ADMIN"));
 
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/api/**").hasAnyRole("ADMIN")
-                .antMatchers("/api/buy").hasAnyRole("USER")
+                .antMatchers("/api/author").hasAnyRole("ADMIN")
                 .antMatchers("/api/books").hasAnyRole("USER")
+                .antMatchers("/api/buy").hasAnyRole("USER")
                 .antMatchers("/api/user").hasAnyRole("USER")
                 .and().formLogin().permitAll();
     }
